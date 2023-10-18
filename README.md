d3a45da3abf3a2fa8746efbe614ac9358b80b8d6
ikeyu0806 <ikeyu0806@gmail.com>
2023-10-18 17:40:13 +0900

    register memo service to server

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 ```
 
 ```bash
+cd proto
 protoc --go_out=../pkg/grpc --go_opt=paths=source_relative memoservice.proto --go-grpc_out=../pkg/grpc --go-grpc_opt=paths=source_relative memoservice.proto
 ```
 
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -8,52 +9,61 @@ import (
 	"os/signal"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	memopb "grpc-memoapp/pkg/grpc"
 )
 
-// type server struct{}
+type memoServer struct {
+	memopb.UnimplementedMemoAPIServer
+}
 
-// func (s *server) GetMemo(ctx context.Context, in *grpcPkg.GetMemoRequest) (*grpcPkg.GetMemoResponse, error) {
-// 	dummyMemo := &grpcPkg.Memo{
-// 		Title:       "Sample Memo",
-// 		Description: "This is a sample memo.",
-// 	}
+func NewMemoServer() *memoServer {
+	return &memoServer{}
+}
 
-// 	response := &grpcPkg.GetMemoResponse{
-// 		Memo: dummyMemo,
-// 	}
+func (s *memoServer) GetMemo(ctx context.Context, req *memopb.GetMemoRequest) (*memopb.GetMemoResponse, error) {
+	dummyMemo := &memopb.Memo{
+		Title:       "Sample Memo",
+		Description: "This is a sample memo.",
+	}
 
-// 	return response, nil
-// }
+	response := &memopb.GetMemoResponse{
+		Memo: dummyMemo,
+	}
 
-// func (s *server) CreateMemo(ctx context.Context, in *grpcPkg.CreateMemoRequest) (*grpcPkg.CreateMemoResponse, error) {
-// 	newMemoID := "12345"
+	return response, nil
+}
 
-// 	response := &grpcPkg.CreateMemoResponse{
-// 		Success: true,
-// 		Id:      newMemoID,
-// 	}
+func (s *memoServer) CreateMemo(ctx context.Context, req *memopb.CreateMemoRequest) (*memopb.CreateMemoResponse, error) {
+	newMemoID := "12345"
 
-// 	return response, nil
-// }
+	response := &memopb.CreateMemoResponse{
+		Success: true,
+		Id:      newMemoID,
+	}
 
-// func (s *server) ListMemos(ctx context.Context, in *grpcPkg.ListMemosRequest) (*grpcPkg.ListMemosResponse, error) {
-// 	dummyMemos := []*grpcPkg.Memo{
-// 		{
-// 			Title:       "Memo 1",
-// 			Description: "This is the first memo.",
-// 		},
-// 		{
-// 			Title:       "Memo 2",
-// 			Description: "This is the second memo.",
-// 		},
-// 	}
+	return response, nil
+}
 
-// 	response := &grpcPkg.ListMemosResponse{
-// 		Memos: dummyMemos,
-// 	}
+func (s *memoServer) ListMemos(ctx context.Context, req *memopb.ListMemosRequest) (*memopb.ListMemosResponse, error) {
+	dummyMemos := []*memopb.Memo{
+		{
+			Title:       "Memo 1",
+			Description: "This is the first memo.",
+		},
+		{
+			Title:       "Memo 2",
+			Description: "This is the second memo.",
+		},
+	}
 
-// 	return response, nil
-// }
+	response := &memopb.ListMemosResponse{
+		Memos: dummyMemos,
+	}
+
+	return response, nil
+}
 
 func main() {
 	port := 8080
@@ -63,6 +73,10 @@ func main() {
 	}
 
 	s := grpc.NewServer()
+
+	memopb.RegisterMemoAPIServer(s, NewMemoServer())
+
+	reflection.Register(s)
 
 	go func() {
 		log.Printf("start gRPC server port: %v", port)
