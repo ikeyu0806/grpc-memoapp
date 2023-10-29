ee0e297e2a5e5129cfd0d6a2e9ec17165d0ffaeb
ikeyu0806 <ikeyu0806@gmail.com>
2023-10-29 15:29:51 +0900

    implement grpc client cmd

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	memopb "grpc-memoapp/pkg/grpc"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./main.go <memoID>")
+		return
+	}
+
+	conn, err := grpc.Dial(
+		"grpc-memoapp:8080", // docker-composeのサービス名とポートを指定
+		grpc.WithTransportCredentials(insecure.NewCredentials()), // この指定がないとブロックされる
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := memopb.NewMemoAPIClient(conn)
+
+	id := os.Args[1]
+	getMemoRequest := &memopb.GetMemoRequest{
+		Id: id,
+	}
+	getMemoResponse, err := client.GetMemo(context.Background(), getMemoRequest)
+	if err != nil {
+		log.Fatalf("Error calling GetMemo: %v", err)
+	}
+	fmt.Printf("GetMemo Response: %+v\n", getMemoResponse)
 }
